6c75eff8143f0e0a62665bb62fcb99d1744feb1e
Hack Reactor Student <student@hackreactor.com>
2017-08-30 19:42:43 -0700

    Video player view now renders the first dummy video in the collection. Template strings not working how it should right now when appending to the end of youtube links.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,21 +10,23 @@ class App extends React.Component {
       <div>
         <nav className="navbar">
           <div className="col-md-6 offset-md-3">
-            <div><h5><em>search</em> view goes here</h5></div>
+            <div><Search/></div>
           </div>
         </nav>
         <div className="row">
           <div className="col-md-7">
-            <div><h5><em>videoPlayer</em> view goes here</h5></div>
+            <div><VideoPlayer videos={this.props.videos[0]}/></div>
           </div>
           <div className="col-md-5">
-            <div><h5><VideoList videos={this.props.videos}/><em>videoList</em> view goes here</h5></div>
+            <div><VideoList videos={this.props.videos}/></div>
           </div>
         </div>
       </div>
     );
   }
 }
+
+window.App = App;
 
 /*class App extends React.Component {
 
@@ -55,4 +57,3 @@ class App extends React.Component {
 }*/
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
-window.App = App;
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,17 +1,7 @@
-/*var VideoList = () => (
-  <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-  </div>
-);*/
-
 var VideoList = (props) => (
   <div className="video-list">
     {props.videos.map(video =>
-      <VideoListEntry video={video}  />
+      <VideoListEntry video={video}/>
     )}
   </div>
 );
@@ -25,3 +15,13 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+
+/*var VideoList = () => (
+  <div className="video-list">
+    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  </div>
+);*/
