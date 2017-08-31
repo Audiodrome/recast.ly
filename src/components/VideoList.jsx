7f80e22f126cdd123428b6c6c0c212c86232dc48
Hack Reactor Student <student@hackreactor.com>
2017-08-30 18:47:59 -0700

    Initialized the App with dummy collection array. Displays the dummy videos in the VideoList.

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,18 @@
-var VideoList = () => (
+/*var VideoList = () => (
   <div className="video-list">
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
+  </div>
+);*/
+
+var VideoList = (props) => (
+  <div className="video-list">
+    {props.videos.map(video =>
+      <VideoListEntry video={video}  />
+    )}
   </div>
 );
 
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -1,4 +1,16 @@
-var VideoListEntry = () => (
+var VideoListEntry = (props) => (
+  <div className="video-list-entry media">
+    <div className="media-left media-middle">
+      <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
+    </div>
+    <div className="media-body">
+      <div className="video-list-entry-title">Video Title</div>
+      <div className="video-list-entry-detail">Video Description</div>
+    </div>
+  </div>
+);
+
+/*var VideoListEntry = (props) => (
   <div className="video-list-entry media">
     <div className="media-left media-middle">
       <img className="media-object" src="https://i.ytimg.com/vi/1w8Z0UOXVaY/default.jpg" alt="" />
@@ -8,7 +20,7 @@ var VideoListEntry = () => (
       <div className="video-list-entry-detail">Video Description</div>
     </div>
   </div>
-);
+);*/
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
